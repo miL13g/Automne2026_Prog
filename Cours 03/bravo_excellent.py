37463fe241bd6f32cfefa9eecc408aa25c35407a
miL <1milee999@gmail.com>
2026-09-09 10:56:20 -0400

    bravo_excellent

---
+++ Cours 03/bravo_excellent.py
@@ -0,0 +1,24 @@
+# Auteur : Émile Bousquet
+# Date 09/09/2026
+# Sujet : Exercise à la fin du chapitre 3.4
+
+#1
+a = 5
+b = 3
+c = 10
+d = 7
+p = True
+q = False
+
+#a 
+print(a > b) #True
+#b
+print(b >= c) #False
+#c
+print(not p) #False
+#d
+print((a == b) or (c < d)) #False
+#e
+print(not (a == b) and (d < c)) #True
+#f
+print((a > b) and (b > c) or (d < c)) #True
