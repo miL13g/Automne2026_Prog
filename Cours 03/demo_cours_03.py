cbb5c9c862b9fae866a38e3f9153d75f3e48ca68
miL <1milee999@gmail.com>
2026-09-09 08:27:25 -0400

    update demo cours 3

---
+++ Cours 03/demo_cours_03.py
@@ -0,0 +1,44 @@
+# Auteur : Émile Bousquet
+# Date : 02/09/2026
+# Sujet : Démo cours 03 et exemples du cours 2
+
+# nom_du_professeur = input("Bonjour, quel est le nom de ton professeur?")
+# print(f"Ton professeur se nome : {nom_du_professeur}")
+
+# contenu_du_type = type(nom_du_professeur)
+# print(f"Le type de ma variable est: {contenu_du_type}")
+
+
+
+# age = float(input("quel est ton age:"))
+# age = age + 1
+# print(f"Ton age est : {age}")
+
+# prix = float(input("quel est le prix de la tomate?"))
+# print(f"La tomate a le prix de: {prix}$")
+# prix = prix + 1
+# print(prix)
+
+
+
+# prenom = "David"
+# nom = "Brouillette"
+# ville = "St-tite"
+
+# print(f"Le prénom du prof est {prenom} {nom} et il habite à {ville}")
+# print(prenom, nom, ville, sep=",", end="")
+
+
+
+# valeur = 1.234567
+# print(f"{valeur:.2f}")
+
+animal1 = "Rat"
+animal2 = "Raton laveur"
+animal3 = "Renard"
+
+print("--------------")
+print(f"|{animal1:<12}|")
+print(f"|{animal2:^12}|")
+print(f"|{animal3:>12}|")
+print(f"--------------")
